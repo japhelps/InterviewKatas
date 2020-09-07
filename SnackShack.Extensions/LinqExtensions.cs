84b122c728f817e537f78e6c62ee4aa625fd7cac
Joel Phelps <japhelps@hotmail.com>
2020-09-07 16:24:41 -0500

    Adding a TakeUntil to simplify searching for the order steps in the bins.

---
+++ SnackShack.Extensions/LinqExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Adds extensions for Linq.
+    /// </summary>
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Gets items from the sequence until the item matches the predicate.
+        /// </summary>
+        /// <typeparam name="T">The type contained in the sequence.</typeparam>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="predicate">The predicate used to determine when to stop the sequence.</param>
+        public static IEnumerable<T> TakeUntil<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            return _(); IEnumerable<T> _()
+            {
+                foreach (var item in source)
+                {
+                    yield return item;
+                    if (predicate(item))
+                        yield break;
+                }
+            }
+        }
+    }
+}
